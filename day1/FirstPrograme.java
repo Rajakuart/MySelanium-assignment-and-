22501505356f572c65b20b7e71742c4793e58b08
Rajakuart <rajakumarloud@gmail.com>
2025-03-29 10:13:35 +0000

    Add files via upload
    Day1

---
+++ day1/FirstPrograme.java
@@ -0,0 +1,27 @@
+package week1.day1;
+
+public class FirstPrograme {
+	public static void main(String[] args) {
+		System.out.println("My First programe");
+		String hello = "Hello how are you doing!";
+		System.out.println(hello);
+		byte time=1;
+		short agekid=10;
+		int age=32;
+		long mobile=156565l;
+		float rate=7.587877f;
+		double gbp=0.876767687;
+		char intial = 'T';
+		String name="Raj";
+		boolean male = true;
+		System.out.println(time);
+		System.out.println(agekid);System.out.println(age);
+		System.out.println(mobile);
+		System.out.println(rate);
+		System.out.println(gbp);
+		System.out.println(intial);
+		System.out.println(name);
+		System.out.println(male);
+	}
+
+}
